df238b63111ce1a38a690dbebe3255e1342bc771
Brodie Heywood <brodieheywood@protonmail.ch>
2021-04-03 23:13:50 -0700

    First commit.

--- App.js
+++ App.js
@@ -13,9 +13,8 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
+    flex: 1,
     justifyContent: 'center',
   },
 });
